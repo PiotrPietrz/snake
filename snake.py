ea63a8dc45b7d47a69961657b76280a688b5a57a
githubusername <myuser@dyvenia.com>
2022-12-16 15:24:44 +0100

    Add square movement functionality

--- snake.py
+++ snake.py
@@ -1,13 +1,35 @@
-# a knock-off from the internet to check if it works
 import pygame
+import time
+
 pygame.init()
-dis = pygame.display.set_mode((400, 300))
-pygame.display.update()
-pygame.display.set_caption('Snake game')
-game_over = False
-while not game_over:
-    for event in pygame.event.get():
-        print(event)  # prints out all the actions that take place on the screen
+
+# constants for the window size
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+color = (0, 245, 0)
+# the size of snake head
+snake_1 = 20
+snake_2 = 20
+x = SCREEN_WIDTH / 2
+y = SCREEN_HEIGHT / 2
+
+surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+rect = pygame.Rect(x, y, snake_1, snake_2)
+
+
+run = True
+
+while run:
+    ev = pygame.event.get()
+    for event in ev:
+        if event.type == pygame.QUIT:
+            run = False
+    surface.fill((255, 255, 255))
+    pygame.draw.rect(surface, color, rect)
+    rect = pygame.Rect.move(rect, 0, 10)
+    pygame.display.update()
+
+    time.sleep(0.5)
+
 
 pygame.quit()
-quit()
